a7cec4468236accabef9b9b82422c566b0acf3d1
ritikegov <ritik.kumar@egovernments.org>
2026-07-02 18:42:08 +0530

    update kubectl provider

--- infra-as-code/terraform/sample-aws/main.tf
+++ infra-as-code/terraform/sample-aws/main.tf
@@ -222,13 +222,15 @@ provider "helm" {
 }
 
 provider "kubectl" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  load_config_file       = false
+  lazy_load              = true
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
     command     = "aws"
-  }              
+  }
 }
 
 resource "aws_iam_role_policy" "karpenter_policy" {
